85f74d67ee5e93a3db6dc9d6fa260e0203a664ac
Parisa Sohrabi <parisahs@gmail.com>
2022-09-09 13:06:00 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,63 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useReducer } from "react";
+import "./App.css";
 
 function App() {
+  const [users, setUsers] = useState([]);
+  const [fname, setFname] = useState("");
+  const [lname, setLname] = useState("");
+  const [email, setEmail] = useState("");
+  const formHandler = (e) => {
+    e.preventDefault();
+    // const newUsers = [...users];
+    // newUsers.push({ fname, lname, email });
+    // setUsers(newUsers);
+    // setFname("");
+    // setLname("");
+    // setEmail("");
+  };
+  const initialState = {
+    firstName: {
+      value: "",
+      error: null,
+    },
+    lastName: {
+      value: "",
+      error: null,
+    },
+    email: {
+      value: "",
+      error: null,
+    },
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form action="" className="form" onSubmit={formHandler}>
+        <div className="input-group">
+          <label htmlFor="">First Name</label>
+          <input
+            type="text"
+            value={fname}
+            onChange={(e) => setFname(e.target.value)}
+          />
+        </div>
+        <div className="input-group">
+          <label htmlFor="">Last Name</label>
+          <input
+            type="text"
+            value={lname}
+            onChange={(e) => setLname(e.target.value)}
+          />
+        </div>
+        <div className="input-group">
+          <label htmlFor="">Email</label>
+          <input
+            type="text"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+        </div>
+      </form>
     </div>
   );
 }
